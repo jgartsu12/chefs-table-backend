33d2d6b608bd94cf02fe303a2c27c360b5cf70d5
jgartsu12 <jgartsu12@gmail.com>
2020-03-29 17:37:24 -0400

    created api CRUD

--- chefsBackEnd/menus/api/serializers.py
+++ chefsBackEnd/menus/api/serializers.py
@@ -5,4 +5,4 @@ from menus.models import Menus
 class MenusSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menus
-        fields = ('id', 'title')
+        fields = ('id', 'titles', 'menu_items', 'menu_items_description', 'soup_prices', 'all_food_but_soups_prices')
--- chefsBackEnd/menus/models.py
+++ chefsBackEnd/menus/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Menus(models.Model):
-
     BREAKFAST_MENU = 'BREAKFAST MENU'
     LUNCH_MENU = 'LUNCH MENU'
     SOUP_MENU = 'SOUP MENU'
@@ -17,3 +16,28 @@ class Menus(models.Model):
         choices=MENU_TITLES,
         default=SOUP_MENU
     )
+
+    menu_items = models.CharField(max_length=120, default='added food or soup item here')
+    menu_items_description = models.TextField(default='describe food or soup item here')
+
+    SMALL_SOUP_PRICE = '$4.00'
+    MEDIUM_SOUP_PRICE = '6.80'
+    LARGE_SOUP_PRICE = '$12.06'
+
+    SOUP_SIZE_PRICES = [
+        (SMALL_SOUP_PRICE, '$4.00'),
+        (MEDIUM_SOUP_PRICE, '$6.80'),
+        (LARGE_SOUP_PRICE, '$12.06')
+    ]
+
+    soup_prices = models.CharField (
+        null= True,
+        blank=True,
+        max_length=150,
+        choices=SOUP_SIZE_PRICES,
+        default=MEDIUM_SOUP_PRICE
+    )
+
+    all_food_but_soups_prices = models.DecimalField(null=True, blank=True, max_digits=4, decimal_places=2)
+
+
