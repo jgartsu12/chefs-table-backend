fe2925fe3b8892975b0c1bced17dbda5b3a5110a
jgartsu12 <jgartsu12@gmail.com>
2020-03-29 14:52:24 -0400

    created models

--- chefsBackEnd/chefsBackEnd/settings.py
+++ chefsBackEnd/chefsBackEnd/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
     'menus',
 
-    'pillow',
+    # 'pillow',
 ]
 
 MIDDLEWARE = [
--- chefsBackEnd/menus/models.py
+++ chefsBackEnd/menus/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Menus(models.Model):
+    def __init__(self, titles):
+        self.titles = titles
+
+    BREAKFAST_MENU = 'BREAKFAST MENU'
+    LUNCH_MENU = 'LUNCH MENU'
+    SOUP_MENU = 'SOUP MENU'
+
+    MENU_TITLES = [
+        (BREAKFAST_MENU, 'BREAKFAST MENU'),
+        (LUNCH_MENU, 'LUNCH MENU'),
+        (SOUP_MENU, 'SOUP MENU')
+    ]
+
+    titles = models.CharField (
+        max_length=150,
+        choices=MENU_TITLES,
+        default=SOUP_MENU
+    )
