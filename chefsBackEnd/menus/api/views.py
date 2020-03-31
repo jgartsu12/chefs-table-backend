036dd754b17380d5cac0c98bae774ad9da89f16e
jgartsu12 <jgartsu12@gmail.com>
2020-03-31 08:24:40 -0400

    added rest-auth

--- chefsBackEnd/menus/api/views.py
+++ chefsBackEnd/menus/api/views.py
@@ -1,3 +1,5 @@
+from rest_framework import permissions
+
 from rest_framework.generics import (
     ListAPIView,
     RetrieveAPIView,
@@ -12,44 +14,55 @@ from menus.models import SoupMenu
 from .serializers import MenusSerializer
 from .serializers import SoupsSerializer
 
+
+
 class MenusListView(ListAPIView):
     queryset = FoodMenus.objects.all()
     serializer_class = MenusSerializer
+    permission_classes = (permissions.AllowAny, )
 
 class MenusDetailView(RetrieveAPIView):
     queryset = FoodMenus.objects.all()
     serializer_class = MenusSerializer
+    permission_classes = (permissions.AllowAny, )
 
 class MenusCreateView(CreateAPIView):
     queryset = FoodMenus.objects.all()
     serializer_class = MenusSerializer
+    permission_classes = (permissions.IsAuthenticated, )
 
 class MenusUpdateView(UpdateAPIView):
     queryset = FoodMenus.objects.all()
     serializer_class = MenusSerializer
+    permission_classes = (permissions.IsAuthenticated, )
 
 class MenusDeleteView(DestroyAPIView):
     queryset = FoodMenus.objects.all()
     serializer_class = MenusSerializer
+    permission_classes = (permissions.IsAuthenticated, )
 
 # Soups
 class SoupsListView(ListAPIView):
     queryset = SoupMenu.objects.all()
     serializer_class = SoupsSerializer
+    permission_classes = (permissions.AllowAny, )
 
 class SoupsDetailView(RetrieveAPIView):
     queryset = SoupMenu.objects.all()
     serializer_class = SoupsSerializer
+    permission_classes = (permissions.AllowAny, )
 
 class SoupsCreateView(CreateAPIView):
     queryset = SoupMenu.objects.all()
     serializer_class = SoupsSerializer
+    permission_classes = (permissions.IsAuthenticated, )
 
 class SoupsUpdateView(UpdateAPIView):
     queryset = SoupMenu.objects.all()
     serializer_class = SoupsSerializer
+    permission_classes = (permissions.IsAuthenticated, )
 
 class SoupsDeleteView(DestroyAPIView):
     queryset = SoupMenu.objects.all()
     serializer_class = SoupsSerializer
-
+    permission_classes = (permissions.IsAuthenticated, )
